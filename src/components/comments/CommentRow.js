4ebfd7fa4ebf78dbc70a81098ae7eeb479873408
Piero Pretto <ppretto@amplymedia.com>
2020-06-04 19:51:03 -0500

    create components

---
+++ src/components/comments/CommentRow.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const Post = (post) => {
+
+    return (
+        <div className="post">
+            <div className="row">
+                <div className="col">
+                    <ProfileIcon/>
+                    <ProfileName/>
+                    <div>{post.timestamp}</div>
+                    <HamburgerMenu/>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col">
+                    <PostText/>
+                    <PostCategories/>
+                    <PostContent/>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col">
+                    <PostToolBar/>
+                    <div>{post.like_count}</div>
+                    <PostComments/>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default connect()(Post);
